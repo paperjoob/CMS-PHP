67f88e6f5075804adbd76da7ccb20f23228d96f0
See Yang <seeyang24@gmail.com>
2019-11-24 16:28:11 -0600

    adding posts.php

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -8,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">CMS Admin</a>
+                <a class="navbar-brand" href="#">CMS Admin</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
@@ -32,7 +32,7 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li>
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown">
@@ -40,7 +40,7 @@
                         </a>
                         <ul id="posts_dropdown" class="collapse">
                             <li>
-                                <a href="#">View All Posts</a>
+                                <a href="./posts.php">View All Posts</a>
                             </li>
                             <li>
                                 <a href="#">Add Posts</a>
@@ -48,7 +48,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i>Categories</a>
+                        <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i>Categories</a>
                     </li>
                     <li class="active">
                         <a href="blank-page.html"><i class="fa fa-fw fa-file"></i>Comments</a>
---
+++ admin/posts.php
@@ -0,0 +1,60 @@
+<?php include "includes/adminheader.php" ?>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php" ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Posts
+                            <small>Author</small>
+                        </h1>
+                        <table class="table table-bordered table-hover"> 
+                            <thead>
+                                <tr>
+                                    <th>Post ID</th>
+                                    <th>Title</th>
+                                    <th>Author</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>10</td>
+                                    <td>Boostrap</td>
+                                    <td>BS Framework</td>
+                                    <td>Bootstrap</td>
+                                    <td>Status</td>
+                                    <td>Image</td>
+                                    <td>Tags</td>
+                                    <td>Comments</td>
+                                    <td>Date</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+        <!-- Include footer.php -->
+        <?php include "includes/admin_footer.php" ?>
