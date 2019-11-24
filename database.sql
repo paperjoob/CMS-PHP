0086d51dc51ec6492c1c6fa126619fc259c88131
See Yang <seeyang24@gmail.com>
2019-11-23 19:12:03 -0600

    added search functionality

--- database.sql
+++ database.sql
@@ -10,3 +10,19 @@ SELECT * FROM `categories`;
 
 -- Insert into the categories table
 INSERT INTO `categories` (`cat_title`) VALUES ('Bootstrap'), ('Javascript'), ('PHP'), ('Java');
+
+-- POSTS TABLE
+CREATE TABLE posts (
+    post_id INT(3) PRIMARY KEY AUTO_INCREMENT,
+    post_category_id INT(3),
+    post_title varchar(255),
+    post_author varchar(255),
+    post_date date,
+    post_image text,
+    post_content text,
+    post_tags varchar(255),
+    post_comment_count INT(11),
+    post_status varchar(255) DEFAULT draft
+);
+
+INSERT INTO `posts` (`post_id`, `post_category_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`) VALUES (NULL, '1', 'Learning PHP Course', 'See Yang', '2019-11-23', NULL, 'I really like this course!', 'see, javascript, php', '1', 'draft');
